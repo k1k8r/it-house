6b3ea8bc8d283d7ac6aae6cb4da74accc155ccb7
k1k8r <andreyzadonsky02@mail.ru>
2021-05-25 21:48:34 +0300

    finished media block

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  public title = 'Главная страница';
+
   constructor() { }
 
   public ngOnInit(): void {
