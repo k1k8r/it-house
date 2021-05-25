bc2ea1f370ec354e8f937c13087d26152bad8957
k1k8r <andreyzadonsky02@mail.ru>
2021-05-25 22:17:46 +0300

    Removed routing for core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { CoreRoutingModule } from './core-routing.module';
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
-    CoreRoutingModule,
   ],
 })
 export class CoreModule { }
